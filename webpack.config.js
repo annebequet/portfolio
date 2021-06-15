0e76872a34d4446bb5af143486242629c30b92aa
annebequet <a.bequet@live.fr>
2021-06-15 17:54:35 +0200

    rename build

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
 "entry": "./src/app.js",
 
  "output": {
-   "path": __dirname + '/build',
+   "path": __dirname + '/docs',
    "filename": "main.js"
  },
  devServer: {
-   contentBase: path.join(__dirname, 'build')
+   contentBase: path.join(__dirname, 'docs')
  },
  "module": {
    "rules": [
