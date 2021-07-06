e8c938cee0ad56f50404f4789d2e87f2c2f8e8ee
annebequet <a.bequet@live.fr>
2021-07-06 17:26:50 +0200

    texts projects

--- src/data/data-projects.js
+++ src/data/data-projects.js
@@ -17,7 +17,7 @@ const dataProjects = [
                 width:'100%',
             }
         ],
-        description: 'Qui sit enim veniam et nisi reprehenderit proident. Exercitation sit qui quis sit labore sint aute ipsum mollit. Qui cupidatat do quis sint et minim dolore ipsum amet deserunt deserunt ad quis sunt. Duis fugiat proident ex adipisicing amet dolore esse.',
+        description: "The Markus application offers a management tool for restaurants: human resources (work contracts, schedules...), and inventory management (inventory, technical sheets, forecasting of future activity...). It can also be accompanied by a custom website made for the restaurant (see project 2). I arrived on this project during the implementation of the V0 and participated in a redesign of the front-end to ensure a more robust and maintainable application.",
         links: [
             {
                 name: 'Site',
@@ -25,7 +25,7 @@ const dataProjects = [
             },
 
         ],
-        technologies: ['ReactJ Native', 'Django'],
+        technologies: ['React Native', 'Django'],
         status: 'Internship',
         date: 'December 2020 - March 2021'
     },
@@ -42,7 +42,7 @@ const dataProjects = [
                 width: '100%',
             }
         ],
-        description: 'Qui sit enim veniam et nisi reprehenderit proident. Exercitation sit qui quis sit labore sint aute ipsum mollit. Qui cupidatat do quis sint et minim dolore ipsum amet deserunt deserunt ad quis sunt. Duis fugiat proident ex adipisicing amet dolore esse.',
+        description: "I realized the front-end part of this website for a restaurant owner, who wanted a new website to take orders. In collaboration with a colleague responsible for the back-end, we managed this project from its implementation to its deployment, while generating from this site templates that can be customized to the taste of other restaurant owners.",
         links: [
             {
                 name: 'Site',
@@ -56,7 +56,7 @@ const dataProjects = [
     {
         id: 3,
         name: "Odyssea Quiz",
-        subtitle:'A grandiose perspective into the nothingness',
+        subtitle:'In the mood to challenge your knowledge ?',
         imgFrontPage: olokosoHome,
         imgDetailsBackgroundColor: 'transparent',
         imgDetailsPage: [
@@ -66,7 +66,7 @@ const dataProjects = [
                 width: '100%',
             }
         ],
-        description: 'Qui sit enim veniam et nisi reprehenderit proident. Exercitation sit qui quis sit labore sint aute ipsum mollit. Qui cupidatat do quis sint et minim dolore ipsum amet deserunt deserunt ad quis sunt. Duis fugiat proident ex adipisicing amet dolore esse.',
+        description: "Odyssea Quiz is an end of study project, realized in a team of four developers. We went through all the steps of development, from the design to the deployment. If this website is one of my first realizations, it also has a more personal dimension. As a former teacher, student and user of e-learning platforms, I was able to observe how much a well-designed pedagogy as well as a playful and dynamic approach are essential to learn something well. Most of the time, it is not by reading an encyclopedia that one develops knowledge but by doing, practising, repeating, and having fun. In this context, web applications offer very interesting tools. From these observations Odyssea Quiz was born! The objective of this application is to propose quizzes classified by categories on the marine theme, with and a personal account to keep track of your progresses, and the implementation of a spaced repetition algorithm to better the learning process.",
         links: [
             {
                 name: 'Site',
@@ -79,7 +79,7 @@ const dataProjects = [
         ],
         technologies: ['ReactJs', 'Symfony'],
         status:'Graduation project',
-        date: 'June 2020 - August 2020'
+        date: 'July 2020 - August 2020'
     }
 ];
 
